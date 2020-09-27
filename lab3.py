47f62c227abe02dbc74b794ff71f06d4db431105
Michael Blanchard <mblanchard@spock.highpoint.edu>
2020-09-27 18:22:53 -0400

    working

--- lab3.py
+++ lab3.py
@@ -12,25 +12,34 @@ __status__ = ""
 
 def main():
    print("Enter two sets as 1,2,3 and a,b,c etc. Then Specify the function type. Enter 1 for one to one, or 2 for onto.")
-   set1list = input("Set One: ")
-   set2list = input("Set Two: ")
-   function = input("Function Type: ")
+   print("Enter with single quotations(' ') \n")
+
+   set1list = str(input("Set One: "))
+   set2list = str(input("Set Two: "))
+   function = int(input("Function Type: "))
 
    set1 = set1list.split(",")
    for i in range(0, len(set1)):
       set1[i] = set1[i].strip()
    
    set2 = set2list.split(",")
-   for i in range(0, len(set2):
+   for i in range(0, len(set2)):
       set2[i] = set2[i].strip()
    
-   if function == "x":
-      if len(set1) == len(set2):
+   if function == 1:
+      if len(set1) > len(set2):
+         print("For One to One, set one can not be bigger than set two. However, set two can be bigger than set one.")
+      for i in range(len(set1)):
+         print(set1[i] + " -> " + set2[i])
+   
+   if function == 2:
+      if len(set1) > len(set2):
          for i in range(len(set1)):
-            print(set1[i] + " -> " + set2[i])
-      else:
-         print("For One to One, both sets must be the same size.")
-   
-   
+            index = i
+            if index >= len(set2):
+               index = len(set2)-1
+            print(set1[i] + " -> " + set2[index])
+      if len(set1) < len(set2):
+         print("For onto, set two must be smaller or equal to set one.")
 if __name__ == "__main__":
    main()
